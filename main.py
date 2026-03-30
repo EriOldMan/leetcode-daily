3925f89ea9938fe6a8988b296033d1aca527753f
EriOldMan <erioldman@gmail.com>
2026-03-30 20:05:07 +0300

    2026-03-30✅

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 class Solution:
-    def canBeEqual(self, s1: str, s2: str) -> bool:
-        return (s1[0] == s2[0] or s1[0] == s2[2]) and (s1[1] == s2[1] or s1[1] == s2[3]) and (s1[2] == s2[2] or s1[2] == s2[0]) and (s1[3] == s2[3] or s1[3] == s2[1])
-    
+    def checkStrings(self, s1: str, s2: str) -> bool:
+        even_letters_s1 = s1[::2]
+        odd_letters_s1 = s1[1::2]
+        even_letters_s2 = s2[::2]
+        odd_letters_s2 = s2[1::2]
+
+        # I didn't know we could use libs
+        even_check = False
+        odd_check = False
+
+        for i in even_letters_s1:
+            if i in even_letters_s2:
+                even_letters_s2 = even_letters_s2.replace(i, '', 1)
+        if even_letters_s2 == '':
+            even_check = True
+
+        for i in odd_letters_s1:
+            if i in odd_letters_s2:
+                odd_letters_s2 = odd_letters_s2.replace(i, '', 1)
+        if odd_letters_s2 == '':
+            odd_check = True
+            
+        return even_check and odd_check
