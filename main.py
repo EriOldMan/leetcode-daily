61ed5486b6e09aead7a4f37c384e0091f8bd63f9
EriOldMan <erioldman@gmail.com>
2026-04-06 23:17:15 +0300

    874. Walking Robot Simulation

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+from typing import List
+
 class Solution:
-    def judgeCircle(self, moves: str) -> bool:
-        left = [n for n in moves if n == 'L']
-        right = [n for n in moves if n == 'R']
-        up = [n for n in moves if n == 'U']
-        down = [n for n in moves if n == 'D']
-        return len(left) == len(right) and len(up) == len(down)
+    def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
+        current_point = [0, 0]
+        furthest_point = [0, 0]
+        direction_map = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        current_direction = 0
+        obstacle_set = {(x, y ) for x, y in obstacles}
+
+        for command in commands:
+            if command == -1:
+                current_direction = (current_direction + 1) % 4       
+            elif command == -2:
+                current_direction = (current_direction - 1) % 4
+            else:
+                for _ in range(command):
+                    next_point = (current_point[0] + direction_map[current_direction][0], current_point[1] + direction_map[current_direction][1])
+                    if next_point in obstacle_set:
+                        break
+                    else:
+                        current_point[0] = next_point[0]
+                        current_point[1] = next_point[1]
+                        if current_point[0] * current_point[0] + current_point[1] * current_point[1] > furthest_point[0] * furthest_point[0] + furthest_point[1] * furthest_point[1]:
+                            furthest_point = current_point[:]
+
+        return furthest_point[0] * furthest_point[0] + furthest_point[1] * furthest_point[1]
